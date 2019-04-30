28ecf746828a2bd3546ec6a398235dbfcb813592
ajbau1 <ajbau1@student.monash.edu>
2019-04-30 11:34:26 +1000

    hellow world stuff in crate TFW

---
+++ createTFW/genHelloWorld.m
@@ -0,0 +1,10 @@
+img=imread('hello world.png');
+logical=img(:,:,1)==0;
+idx=find(logical==1);
+
+array=zeros(1,60*60);
+for i=idx
+    array(i)=mod(i,60)./30;
+end
+array=array-1;
+createTFW(array,'hello world.tfw')
