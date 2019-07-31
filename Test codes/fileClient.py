2bd57635a4670b45d736d07100aec49685a8fc1b
ajbau1 <ajbau1@student.monash.edu>
2019-07-31 10:09:53 +1000

    file server stuff

--- Test codes/fileClient.py
+++ Test codes/fileClient.py
@@ -1,1 +1,22 @@
 #from https://stackoverflow.com/questions/27241804/sending-a-file-over-tcp-sockets-in-python
+
+import socket               # Import socket module
+
+s = socket.socket()         # Create a socket object
+host = socket.gethostname() # Get local machine name
+port = 12345                 # Reserve a port for your service.
+
+s.connect((host, port))
+#s.send("Hello server!")
+f = open('testFile.txt','rb')
+print('Sending...')
+l = f.read(1024)
+while (l):
+    print('Sending...')
+    s.send(l)
+    l = f.read(1024)
+f.close()
+s.shutdown()
+print("Done Sending")
+print(s.recv(1024))
+s.close                     # Close the socket when done
--- Test codes/fileServer.py
+++ Test codes/fileServer.py
@@ -1,1 +1,24 @@
 #from https://stackoverflow.com/questions/27241804/sending-a-file-over-tcp-sockets-in-python
+
+import socket               # Import socket module
+
+s = socket.socket()         # Create a socket object
+host = socket.gethostname() # Get local machine name
+port = 12345                 # Reserve a port for your service.
+s.bind((host, port))        # Bind to the port
+f = open('testRCV.txt','wb')
+s.listen(5)                 # Now wait for client connection.
+while True:
+    c, addr = s.accept()     # Establish connection with client.
+    c.settimeout(5)
+    print('Got connection from', addr)
+    print("Receiving...")
+    l = c.recv(1024)
+    while (l):
+        print("Receiving...")
+        f.write(l)
+        l = c.recv(1024)
+    f.close()
+    print("Done Receiving")
+    #c.send('Thank you for connecting')
+    c.close()                # Close the connection
