b2d54b4588508fb702a3e96c8a9e2e1a5aecd884
Jarred27 <jarred.paola@gmail.com>
2019-08-14 09:49:48 +1000

    tidying repo
    removing unnecessary documents and files

--- Test codes/dummyFun.py
+++
@@ -1,7 +0,0 @@
-from fabric.api import run
-
-
-def hello(who = "world"):
-    print("Hello {who}!".format(who=who))
-
-
--- Test codes/matlabtest/mymat.m
+++
@@ -1,4 +0,0 @@
-import py.mypython.*
-
-arr = [[1,2,3];[4,5,6]];
-arg = transpose(arr)
