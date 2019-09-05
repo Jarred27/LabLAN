3897a8f0655ee2bff2343dc42b20d527d7968328
ajbau1 <ajbau1@student.monash.edu>
2019-09-05 21:31:21 +1000

    GUI refresh status functionality, low level m functions

---
+++ client/directScripts/AWGsampleRateQuery.m
@@ -0,0 +1,7 @@
+function frequency = AWGsampleRateQuery()
+%AWGSAMPLERATEQUERY Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
