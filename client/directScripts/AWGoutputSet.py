961c8ba66840e8e97ecb5430167e7d7b6d431ec0
ajbau1 <ajbau1@student.monash.edu>
2019-09-11 10:09:49 +1000

    AWGoutputSet.py works

--- client/directScripts/AWGoutputSet.py
+++ client/directScripts/AWGoutputSet.py
@@ -1,11 +1,14 @@
-def function AWGoutputSet(channel, ONorOFF)
+import write
+
+def AWGoutputSet(channel,ONorOFF):
 	# AWGoutputSet Summary of this function goes here
 	# channel is which channel you want 
 	# ONorOFF is either 1 or 0
 	
-	import write.py
+	
 	
 	AWGadd = "TCPIP0::localhost::inst1::INSTR"
 	Command = ":OUTP" + str(channel) + " " + str(ONorOFF)
 	
-	return = write(AWGadd,Command)
+	response = write.write(AWGadd,Command)
+	return response
