ccf9849928ddc634d9c6347250a9b948efd0709d
ajbau1 <ajbau1@student.monash.edu>
2019-09-17 15:15:39 +1000

    moved old GUI files into dedicated folder

---
+++ client/directScripts/transferFile.py
@@ -0,0 +1,65 @@
+import getConfigs
+import socket
+import sys
+
+#  moves file into dedicated folder on server, needs to be pushed onto AWG with uploadFile.py
+
+def transferFile(filePath,destFilename):
+    #load settings
+    [TCP_IP,TCP_PORT,BUFFER_SIZE,connectionTimeout]=getConfigs.getConfigs().split(", ")
+    TCP_PORT=int(TCP_PORT)
+    BUFFER_SIZE=int(BUFFER_SIZE)
+    connectionTimeout = int(connectionTimeout)
+
+    s = socket.socket()
+	
+    #connect to server
+    errorFlag = s.connect_ex((TCP_IP, TCP_PORT))
+    if errorFlag != 0:
+        returnString="err hostNotFoundErr#:"+str(errorFlag)
+        s.close()
+        return returnString
+	message="file, txRequest, " + destFilename, "UTF8"
+    s.send(message.encode('UTF8'))
+    #print('waiting for server')
+    response = s.recv(BUFFER_SIZE)
+    if response != b'file, rxReady':
+        #print("handshake error")
+        #print(response.decode("utf-8"))
+        s.close
+        return "err handshake error"
+    file = open(filePath, 'rb')
+    #print('Sending...')
+    line = file.read(BUFFER_SIZE)
+    while (line):
+        #print('Sending...')
+        s.send(line)
+        line = file.read(BUFFER_SIZE)
+    #print("Done Sending")
+    file.close()
+    # s.shutdown(0)
+    try:
+        response = s.recv(BUFFER_SIZE)
+        response=response.decode("utf-8")
+        arr=response.split(", ")
+        #print(arr)
+        if arr[2]!="0":
+            returnString=response#"err server returned error"
+        else:
+            returnString="success"
+    except:
+        returnString="err server response lost"
+    #print("Exiting")
+    s.close  # Close the socket when done
+    return returnString
+
+if __name__=="__main__":
+    if len(sys.argv) == 3:
+        filePath = sys.argv[1]
+        destFilename = sys.argv[2]
+        result = transferFile(filePath,destFilename)
+        print(result)
+        sys.exit(result.split(" ")[0]=="err")
+    else:
+        print("expected 2 arguments got "+str(len(sys.argv)-1)+"\ncorrect usage: transferFile.py filePath destFilename")
+        sys.exit(1)
---
+++ client/directScripts/uploadFile.py
@@ -0,0 +1,59 @@
+import getConfigs
+import socket
+import sys
+
+# description here
+
+def uploadFile(fileName,channelNum):
+    #load settings
+    [TCP_IP,TCP_PORT,BUFFER_SIZE,connectionTimeout]=getConfigs.getConfigs().split(", ")
+    TCP_PORT=int(TCP_PORT)
+    BUFFER_SIZE=int(BUFFER_SIZE)
+    connectionTimeout = int(connectionTimeout)
+
+    #define string to send
+    messageString = "file, upload, " + fileName + ", " + channelNum
+    formattedMessage=messageString.encode('UTF8')
+
+    #bind port
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except:
+        return "err socketConfigError"
+
+    #connect to server
+    errorFlag = s.connect_ex((TCP_IP, TCP_PORT))
+    if errorFlag != 0:
+        returnString="err hostNotFoundErr#:"+str(errorFlag)
+        s.close()
+        return returnString
+
+    #send message
+    s.send(formattedMessage)
+    data = s.recv(BUFFER_SIZE)
+    if not data:
+        returnString="err noResponse"
+    else:
+        response = data.decode("utf-8")
+        arr = response.split(", ")
+        if (arr[0]=="file") & (arr[1]=="uploadResult"):
+            if arr[2] == "1":#error flag
+                returnString="err "+arr[3]
+            else:
+                returnString = arr[2]
+        else:
+            returnString="err unexpectedResponse"
+
+    s.close()
+    return returnString
+
+if __name__=="__main__":
+    if len(sys.argv) == 3:
+        fileName = sys.argv[1]
+        channelNum = sys.argv[2]
+        result=uploadFile(fileName,channelNum)
+        print(result)
+        sys.exit(result.split(" ")[0]=="err")
+    else:
+        print("err expected 2 argument got "+str(len(sys.argv)-1))
+        sys.exit(1)
